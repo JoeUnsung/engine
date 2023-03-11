78be1b95c36f300f66aa6655a38481da3a53f527
Joe <esj1020@gmail.com>
2023-03-11 14:16:32 +0900

    feat: 셀레니움 세팅

--- main.py
+++ main.py
@@ -6,22 +6,50 @@
 import selenium
 from selenium import webdriver
 
-import bs4
+from bs4 import BeautifulSoup as bs
 import pandas
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 
 def run():
     print("Start Engine")
+    # path_driver = '/Users/joe/github/engine/app/chromedriver_mac64_111/chromedriver'
+    # path_driver_arm = '/Users/joe/github/engine/app/chromedriver_mac_arm64/chromedriver'
 
     path_driver = '/Users/joe/github/engine/app/chromedriver_mac64/chromedriver'
-    path_driver_arm = '/Users/joe/github/engine/app/chromedriver_mac_arm64/chromedriver'
 
-    driver = webdriver.Chrome(path_driver)
-    driver.implicitly_wait(3)
+    url = 'https://www.courtauction.go.kr/' ## https://를 붙여줘야 에러가 안남
+    naver = 'https://www.naver.com/' ## https://를 붙여줘야 에러가 안남
 
-    driver.get('https://google.com')
+    # options = Options()
+    # options.add_argument("start-maximized")
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # driver.get(naver)
+
+    # driver = webdriver.Chrome(executable_path=path_driver)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print("Current session is {}".format(driver.session_id))
+    # driver.get(naver)
+    driver.implicitly_wait(10)
+    driver.get(url)
+    # driver.get(naver)
+
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+
+    print(soup)
+
+    # # driver.quit()
+    # try:
+    #     driver.get(naver)
+    # except Exception as e:
+    #     print(e.message)
+
 
 
 
--- readme.md
+++ readme.md
@@ -18,3 +18,19 @@ pip install -r requirements.txt
 파이참 내에서 인터프리터 설정시 existing 선택 후 특정버전 python 선택
 (new 선택시 새로운 폴더를 생성하여 해당 경로에 라이브러리를 설치하기 때문에 terminal과 pycharm interpreter에서 설치하는 라이브러리가 달라짐)
 
+
+### 디펜던시 파일
+
+크롬드라이버 설치:
+https://sites.google.com/chromium.org/driver/downloads
+(크롬 버전 확인 후 설치 chrome://version/)
+
+
+#### 셀레니움 관련 사이트 모음
+Selenium with Python: https://selenium-python.readthedocs.io/
+SeleniumHQ Documentation: https://www.selenium.dev/documentation/
+Guru99 Selenium Tutorials: https://www.guru99.com/selenium-tutorial.html
+TestProject Selenium Tutorials: https://blog.testproject.io/category/selenium-tutorials/
+BrowserStack Selenium Tutorials: https://www.browserstack.com/guide/selenium-tutorial
+
+위 사이트들은 Selenium을 처음 접하는 분들도 쉽게 이해할 수 있는 내용과 예제들을 제공합니다. 각 사이트들은 다양한 주제에 대한 튜토리얼과 예제를 제공하므로, 차근차근 따라하며 학습하시면 됩니다.
