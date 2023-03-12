6507a401cdb86811f236eefc3ca000d556d94a0a
Joe <esj1020@gmail.com>
2023-03-12 15:52:22 +0900

    feat: 구조 체크중

--- main.py
+++ main.py
@@ -12,56 +12,47 @@ import pandas
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains ## Click 실행을 하기 위해 임포트
+from selenium.webdriver.common.by import By ## CSS셀렉터로 변환하기 위해 임포트
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 
 def run():
     print("Start Engine")
-    # path_driver = '/Users/joe/github/engine/app/chromedriver_mac64_111/chromedriver'
-    # path_driver_arm = '/Users/joe/github/engine/app/chromedriver_mac_arm64/chromedriver'
 
+    ## 접근 URL 정리
     path_driver = '/Users/joe/github/engine/app/chromedriver_mac64/chromedriver'
 
     url = 'https://www.courtauction.go.kr/' ## https://를 붙여줘야 에러가 안남
     naver = 'https://www.naver.com/' ## https://를 붙여줘야 에러가 안남
 
-    # options = Options()
-    # options.add_argument("start-maximized")
-    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    # driver.get(naver)
-
-    # driver = webdriver.Chrome(executable_path=path_driver)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     print("Current session is {}".format(driver.session_id))
-    # driver.get(naver)
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     driver.get(url)
     # driver.get(naver)
-    driver.implicitly_wait(10)
 
     driver.switch_to.frame('indexFrame')
     html = driver.page_source
     print(html)
 
-    print("=============================================")
+    element = driver.find_element(by=By.CSS_SELECTOR,value="img[alt='경매물건']") # 홈화면 > 경매물건
+    actions = ActionChains(driver)
+    actions.move_to_element(element).click().perform() ## 버튼 클릭
+    driver.implicitly_wait(10)
+
+
+
     print("=============================================")
 
     document_element = driver.execute_script("return document.documentElement;")
     print(document_element)
 
     soup = bs(html, 'html.parser')
-
     # print(soup)
 
     driver.quit()
-
-    # # driver.quit()
-    # try:
-    #     driver.get(naver)
-    # except Exception as e:
-    #     print(e.message)
-
 
 
 
